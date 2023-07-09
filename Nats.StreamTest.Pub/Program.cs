a47c815891445b51e56b4b02ad8dee9d0b9ba283
kjun <keisoft.kjun@gmail.com>
2023-07-10 00:22:05 +0900

    초기 커밋.

---
+++ Nats.StreamTest.Pub/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+using STAN.Client;
+
+namespace Nats.StreamTest.Pub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientId = $"pub-{Guid.NewGuid().ToString()}";
+
+            var cf = new StanConnectionFactory();
+            StanOptions options = StanOptions.GetDefaultOptions();
+            options.NatsURL = "nats://localhost:4222";
+
+            using (var c = cf.CreateConnection("test-cluster", clientId, options))
+            {
+                for (int i = 1; i <= 25; i++)
+                {
+                    string message = $"[{DateTime.Now.ToString("hh:mm:ss:fffffff")}] Message {i}";
+                    Console.WriteLine($"Pubish - Sending {message}");
+
+                    c.Publish("nats.streaming.channel", Encoding.UTF8.GetBytes(message));
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Nats.StreamTest.Sub/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+using STAN.Client;
+
+namespace Nats.StreamTest.Sub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Length != 1)
+            //{
+            //    Console.WriteLine("Usage: consumer <clientid>");
+            //    return;
+            //}
+
+            string clientId = "ClientID" + Guid.NewGuid().ToString().Replace("-", "");
+
+            var cf = new StanConnectionFactory();
+
+            StanOptions options = StanOptions.GetDefaultOptions();
+            options.NatsURL = "nats://localhost:4222";
+
+            using (var c = cf.CreateConnection("test-cluster", clientId, options))
+            {
+                var opts = StanSubscriptionOptions.GetDefaultOptions();
+
+                //opts.DeliverAllAvailable();
+                //opts.StartAt(15);
+                //opts.StartAt(TimeSpan.FromSeconds(10));
+                //opts.StartAt(new DateTime(2019, 9, 18, 9, 0, 0));
+                //opts.StartWithLastReceived();
+                //opts.DurableName = "durable";
+
+                var s = c.Subscribe("nats.streaming.channel", opts, (obj, args) =>
+                {
+                    string message = Encoding.UTF8.GetString(args.Message.Data);
+                    Console.WriteLine($"[#{args.Message.Sequence}] {message}");
+                });
+
+                Console.WriteLine($"Subcribe - client id '{clientId}'");
+                Console.ReadKey(true);
+
+                //s.Unsubscribe();
+                c.Close();
+            }
+        }
+    }
+}
